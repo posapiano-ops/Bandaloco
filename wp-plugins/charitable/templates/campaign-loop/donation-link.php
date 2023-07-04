cc54e9b3adb6a7cb860eade516d122ce6eda6f13
posapiano-ops <rfiorito@outlook.it>
2023-07-04 17:50:15 +0200

    refactor directory

--- wp-plugins/charitable/templates/campaign-loop/donation-link.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * This template was deprecated in 1.2.3. Use campaign-loop/donate-link.php instead.
- *
- * @deprecated
- */
-
-charitable_template( 'campaign-loop/donate-link.php', $view_args );
